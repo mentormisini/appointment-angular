f7a56d65762eae71b62807a966345131d94b66a9
Diellza Bytyci <@diellzabytyci>
2022-01-26 23:22:46 +0100

    shfaqja e orarave

--- src/app/oraret/oraret.component.ts
+++ src/app/oraret/oraret.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { OraretService } from '../_services/oraret.service';
 
+import {FormControl} from '@angular/forms';
+
 @Component({
   selector: 'app-oraret',
   templateUrl: './oraret.component.html',
   styleUrls: ['./oraret.component.css']
 })
 export class OraretComponent implements OnInit {
-
-  oraret: string[] = [];
-  constructor(private oraretService: OraretService) { this.getOraret()}
+  toppings = new FormControl();
+ selected:Date;
+ 
+oraret: string[] = [];
+  constructor(private oraretService: OraretService) { this.getOraret();}
 
   ngOnInit(): void {
+    
+    // this.oraretService.getOraret().subscribe(
+    //   response => this.oraret = response.oraret);
+  }
+  onClickDate(){
+    console.log("dataa" + this.selected);
   }
 
-  getOraret(): void{
+  getOraret(){
     this.oraretService.getOraret().subscribe(
-      response => this.shfaqeOraret(response));
+      respon => this.shfaqeOraret(respon));
   
   }
 
   shfaqeOraret(response){
     this.oraret = response;
-    console.log(this.oraret);
+  
+    console.log("sssssss"+response);
   }
 }
